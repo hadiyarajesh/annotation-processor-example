f6f7553eb08dc14919c67c6c6b5230e6efe52512
hadiyarajesh <rajesh.hadiya@outlook.com>
2024-04-28 23:21:27 +0530

    code refactoring

--- app/src/main/java/com/hadiyarajesh/codegeneration/HiltModule.kt
+++ app/src/main/java/com/hadiyarajesh/codegeneration/HiltModule.kt
@@ -1,10 +1,18 @@
 package com.hadiyarajesh.codegeneration
 
+import com.hadiyarajesh.annotations.GenerateBinding
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
+/**
+ * As we're using annotation processor to generate Interface-Implementation binding,
+ * we don't need to write it manually.
+ * When we use [GenerateBinding] annotation, [GenerateBindingProcessor] will generate code
+ * at compile time at following location.
+ * /app/build/generated/source/kapt/debug/com/hadiyarajesh/codegeneration/
+ */
 //@Module
 //@InstallIn(SingletonComponent::class)
 //abstract class MyRepositoryModule {
@@ -12,7 +20,3 @@ import dagger.hilt.components.SingletonComponent
 //    abstract fun bindMyRepository(impl: MyRepositoryImpl): MyRepository
 //}
 
-//interface MyApi {
-//    @GET("users")
-//    suspend fun getUsers(): List<User>
-//}
--- processors/build.gradle.kts
+++ processors/build.gradle.kts
@@ -11,11 +11,19 @@ java {
 
 dependencies {
     implementation(project(":annotations"))
-    implementation(libs.javapoet)
-    implementation("com.google.dagger:dagger:2.50")
-    compileOnly(libs.google.auto.service)
+    implementation(libs.dagger)
+
+    implementation(libs.javapoet) {
+        because("A Java API for generating .java source files")
+    }
+
+    compileOnly(libs.google.auto.service) {
+        because("A configuration/metadata generator for java.util.ServiceLoader-style service providers")
+    }
     kapt(libs.google.auto.service)
 
-    compileOnly(libs.incap.core)
+    compileOnly(libs.incap.core) {
+        because("Helper library and annotation processor for building incremental annotation processors")
+    }
     kapt(libs.incap.processor)
 }
